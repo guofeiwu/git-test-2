3df3be3a5edfd3f8069e2ade165b5a0ce9107bd6
wuguofei <guofei_wu@163.com>
2017-08-12 23:06:48 +0800

    这是一个新创建的test类

---
+++ src/com/wgf/git/Application.java
@@ -0,0 +1,10 @@
+package com.wgf.git;
+
+/**
+ * Created by GuofeiWu on 2017/8/12.
+ */
+public class Application {
+    public static void main(String[] args){
+        System.out.println("this is a test application");
+    }
+}
