3755636bcacd1c1b4dde87c468cd168b792b7123
Mason <guofei_wu@163.com>
2019-11-13 08:58:06 +0800

    哈哈哈发发发法法安抚急急急toString add
    hashCode add and euqals add

--- src/com/wgf/git/bean/Branch3.java
+++ src/com/wgf/git/bean/Branch3.java
@@ -6,4 +6,18 @@ package com.wgf.git.bean;
  * @Date 2019/11/12 16:05
  **/
 public class Branch3 {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
